00a7e8ba63d00ba4ca5452a6e7912c0748917fac
Julian Martinez <julianmsbar@gmail.com>
2022-08-30 19:09:22 -0300

    Agregué mi archivo de scripts y calculadora simple

---
+++ scripts/myscripts.js
@@ -0,0 +1,27 @@
+
+function multiplicar() {
+    
+    const x = parseInt(document.getElementById('valor1').value)
+
+    const y = parseInt(document.getElementById('valor2').value)
+    
+    document.getElementById('result').innerHTML = (x*y);
+
+    console.log(x,y)
+
+}
+
+
+
+
+
+
+// if(valor2 == '15'){
+//     y = 15;
+//     }else if(valor2 == '30'){
+//     y = 30;
+//     }else if(valor2 == '25'){
+//     y = 25;
+//     }else if(valor2 == '40'){
+//     y = 40;
+//     }

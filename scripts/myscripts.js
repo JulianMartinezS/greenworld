143e8696bce895c7d691c3836a20480a6c8c239f
Julian Martinez <julianmsbar@gmail.com>
2022-09-06 20:49:43 -0300

    Dimensioné las imágenes, el form y quité error de consigna

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,27 +1,62 @@
 
-function multiplicar() {
+let serviciosChicos = [
+    {servicio: "Corte cesped" , precio: 15},
+    {servicio: "Poda parque" , precio: 30 },
+    {servicio: "Diseño jardin" , precio: 25 },
+    {servicio: "Mantenimiento general" , precio: 40 },
+]
+
+let serviciosMedianos = [
+    {servicio: "Corte cesped" , precio: 12},
+    {servicio: "Poda parque" , precio: 25 },
+    {servicio: "Diseño jardin" , precio: 22 },
+    {servicio: "Mantenimiento general" , precio: 33 },
+]
+
+let serviciosGrandes = [
+    {servicio: "Corte cesped" , precio: 10},
+    {servicio: "Poda parque" , precio: 21 },
+    {servicio: "Diseño jardin" , precio: 18 },
+    {servicio: "Mantenimiento general" , precio: 29 },
+]
+
+let serviciosExtraGrande = [
+    {servicio: "Corte cesped" , precio: 7 },
+    {servicio: "Poda parque" , precio: 18 },
+    {servicio: "Diseño jardin" , precio: 15 },
+    {servicio: "Mantenimiento general" , precio: 27 },
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function multiplicar() {
     
-    const x = parseInt(document.getElementById('valor1').value)
+//     const x = parseInt(document.getElementById('valor1').value)
 
-    const y = parseInt(document.getElementById('valor2').value)
+//     const y = parseInt(document.getElementById('valor2').value)
     
-    document.getElementById('result').innerHTML = (x*y);
+//     document.getElementById('result').innerHTML = (x*y);
 
-    console.log(x,y)
+//     console.log(x,y)
 
-}
-
-
-
-
-
-
-// if(valor2 == '15'){
-//     y = 15;
-//     }else if(valor2 == '30'){
-//     y = 30;
-//     }else if(valor2 == '25'){
-//     y = 25;
-//     }else if(valor2 == '40'){
-//     y = 40;
-//     }
+// }

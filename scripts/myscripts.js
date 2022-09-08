147e2d93b0f076ce3f266d0f04ac67a5d1cc576c
Julian Martinez <julianmsbar@gmail.com>
2022-09-08 19:47:44 -0300

    Primera preentrega proyecto

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,51 +1,51 @@
 
-let serviciosChicos = [
-    {servicio: "Corte cesped" , precio: 15},
-    {servicio: "Poda parque" , precio: 30 },
-    {servicio: "Diseño jardin" , precio: 25 },
-    {servicio: "Mantenimiento general" , precio: 40 },
-]
+// let serviciosChicos = [
+//     {servicio: "Corte cesped" , precio: 15},
+//     {servicio: "Poda parque" , precio: 30 },
+//     {servicio: "Diseño jardin" , precio: 25 },
+//     {servicio: "Mantenimiento general" , precio: 40 },
+// ]
 
-let serviciosMedianos = [
-    {servicio: "Corte cesped" , precio: 12},
-    {servicio: "Poda parque" , precio: 25 },
-    {servicio: "Diseño jardin" , precio: 22 },
-    {servicio: "Mantenimiento general" , precio: 33 },
-]
+// let serviciosMedianos = [
+//     {servicio: "Corte cesped" , precio: 12},
+//     {servicio: "Poda parque" , precio: 25 },
+//     {servicio: "Diseño jardin" , precio: 22 },
+//     {servicio: "Mantenimiento general" , precio: 33 },
+// ]
 
-let serviciosGrandes = [
-    {servicio: "Corte cesped" , precio: 10},
-    {servicio: "Poda parque" , precio: 21 },
-    {servicio: "Diseño jardin" , precio: 18 },
-    {servicio: "Mantenimiento general" , precio: 29 },
-]
+// let serviciosGrandes = [
+//     {servicio: "Corte cesped" , precio: 10},
+//     {servicio: "Poda parque" , precio: 21 },
+//     {servicio: "Diseño jardin" , precio: 18 },
+//     {servicio: "Mantenimiento general" , precio: 29 },
+// ]
 
-let serviciosExtraGrande = [
-    {servicio: "Corte cesped" , precio: 7 },
-    {servicio: "Poda parque" , precio: 18 },
-    {servicio: "Diseño jardin" , precio: 15 },
-    {servicio: "Mantenimiento general" , precio: 27 },
-]
+// let serviciosExtraGrande = [
+//     {servicio: "Corte cesped" , precio: 7 },
+//     {servicio: "Poda parque" , precio: 18 },
+//     {servicio: "Diseño jardin" , precio: 15 },
+//     {servicio: "Mantenimiento general" , precio: 27 },
+// ]
 
-let serviciosGenerales = [
-    {serviciosChicos},
-    {serviciosMedianos},
-    {serviciosGrandes},
-    {serviciosExtraGrande}
-]
+// let serviciosGenerales = [
+//     {serviciosChicos},
+//     {serviciosMedianos},
+//     {serviciosGrandes},
+//     {serviciosExtraGrande}
+// ]
+
 
 function crearPresupuesto () {
-
-    const presupuestoArea = prompt ("Ingresa el área de tu parque en metros cuadrados. Ej: 42")
     
-    const presupuestoPrecio = prompt ("Ingresa el servicio que deseas. (Corte cespeded, Poda parque, Diseño jardín o Mantenimiento general")
+    let presupuestoArea = prompt ("Ingresa el área de tu parque en metros cuadrados. Ej: 42")
     
+    let presupuestoPrecio = prompt ("Ingresa el servicio que deseas. (Corte cespeded, Poda parque, Diseño jardín o Mantenimiento general")
+    
+    alert ("Su presupuesto personalizado es " + presupuestoArea*presupuestoPrecio + " dolares.")
 }
 
 crearPresupuesto ()
 
-console.log(presupuestoArea)
-console.log(presupuestoPrecio)
 
 
 // let productoSugerido

38dba461b99abb1af6daf72fe19daf84b9e43cd8
Julian Martinez <julianmsbar@gmail.com>
2022-09-17 18:39:32 -0300

    Primera entrega de proyecto

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,166 +1,71 @@
-// function lafuncion () {
 
-// let autos = [' Volkswagen ',' Fiat ',' Chevrolet ',' Mercedes ']
+function lafuncion () {
 
-// console.log(autos)
+let flores = [
+    ' Tulipan ',' Margarita ',' Dalia ',' Lirio '
+]
 
-// const camiones = ['Scania','Ford','Bmw','Renault']
+console.log(flores)
 
-// let favoritos = prompt ("¿Cual es tu marca de vehículos favorita?")
+const arboles = ['Cedro','Pino','Acacia','Roble']
 
-// autos.push (favoritos)
+let favoritos = prompt ("¿Que flores quieres agregarle a tu jardín?")
 
-// alert ("Las marcas favoritas de autos de nuestros usuarios son: "+autos)
+flores.push (favoritos)
 
-// lafuncion ()
+alert ("Tus flores elegidas hasta el momento son: " + flores)
 
-// }
+lafuncion ()
 
-// lafuncion ()
-
-
-
-
-
-
-
-// let serviciosCortes = [
-//     {servicio: "Corte chico" , precio: 30},
-//     {servicio: "Corte mediano" , precio: 25},
-//     {servicio: "Corte grande" , precio: 21},
-//     {servicio: "Corte extra grande" , precio: 18},
-// ]
-
-// let serviciosPodas = [
-//     {servicio: "Poda chico" , precio: 30},
-//     {servicio: "Poda mediano" , precio: 25},
-//     {servicio: "Poda grande" , precio: 21},
-//     {servicio: "Poda extra grande" , precio: 18},
-// ]
-
-// let serviciosDisenio = [
-//     {servicio: "Diseño chico" , precio: 25},
-//     {servicio: "Diseño mediano" , precio: 22},
-//     {servicio: "Diseño grande" , precio: 18},
-//     {servicio: "Diseño extra grande" , precio: 15},
-// ]
-
-// let serviciosMantenimiento = [
-//     {servicio: "Mantenimiento chico" , precio: 40},
-//     {servicio: "Mantenimiento mediano" , precio: 33},
-//     {servicio: "Mantenimiento grande" , precio: 29},
-//     {servicio: "Mantenimiento extra grande" , precio: 27},
-// ]
-
-
-// let serviciosMedianos = [
-//     {servicio: "Corte cesped" , precio: 12},
-//     {servicio: "Poda parque" , precio: 25 },
-//     {servicio: "Diseño jardin" , precio: 22 },
-//     {servicio: "Mantenimiento general" , precio: 33 },
-// ]
-
-// let serviciosGrandes = [
-//     {servicio: "Corte cesped" , precio: 10},
-//     {servicio: "Poda parque" , precio: 21 },
-//     {servicio: "Diseño jardin" , precio: 18 },
-//     {servicio: "Mantenimiento general" , precio: 29 },
-// ]
-
-
-
-// let presupuestoArea = prompt ("Ingresa el área de tu parque en metros cuadrados. Ej: 42")
-
-// let presupuestoTipo = prompt ("Viendo la tabla escribe el precio del servicio que deseas: Ej: 12")
-
-// alert ("Su presupuesto personalizado es " + presupuestoArea*presupuestoTipo + " dolares.")
-
-
-// function tuPresupuesto () {
-
-    
-
-// }
-
-
-// tuPresupuesto ()
-
-
-
-
-
-
-
-
-// function crearPresupuesto () {
-    
-//     let presupuestoArea = prompt ("Ingresa el área de tu parque en metros cuadrados. Ej: 42")
-    
-//     let presupuestoTipo = prompt ("Ingresa el tipo de servicio que deseas. (Corte, Poda, Diseño o Mantenimiento")
-    
-//     if (presupuestoTipo = Corte && presupuestoArea ≤6) {
-//         let buscar = serviciosCortes.find (elemm) => elemm.corteChico 
-//     }
-//     else if () {
-
-//     }
-//     else if () {
-
-//     }
-//     else () {
-
-//     }
-
-
-//     alert ("Su presupuesto personalizado es " + presupuestoArea* + " dolares.")
-// }
-
-// crearPresupuesto ()
-
-// console.log(buscar)
-
-// console.log("hola")
-
-
-
-
-
-
-
-// let productoSugerido
-
-// if (presupuestoPers <5) {productoSugerido=serviciosChicos} 
-// else if (presupuestoPers >5 && presupuestoPers <15 ) {productoSugerido=serviciosMedianos} 
-// else if (presupuestoPers >15 && presupuestoPers <50 ) {productoSugerido=serviciosGrandes} 
-// else {productoSugerido=serviciosExtraGrande}
-
-
-
-
-
-
-
-
-
-
-// function  prompt ('¿Cuantos metros cuadrados tiene tu jardin/parque?')
-
-// if (prompt < 5) = alert ('Tu parque califica como - Chico -')
-
-
-
-
-
-
-
-function multiplicar() {
-    
-    const superficiePers = parseInt(document.getElementById('valor1').value)
-
-    const tipoDeServicio = parseInt(document.getElementById('valor2').value)
-    
-    document.getElementById('result').innerHTML = (superficiePers*tipoDeServicio); 
-
-    console.log(superficiePers, tipoDeServicio)
 
 }
+
+lafuncion ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function multiplicar() {
+    
+//     const superficiePers = parseInt(document.getElementById('valor1').value)
+
+//     const tipoDeServicio = parseInt(document.getElementById('valor2').value)
+    
+//     document.getElementById('result').innerHTML = (superficiePers*tipoDeServicio); 
+
+//     console.log(superficiePers, tipoDeServicio)
+
+// }

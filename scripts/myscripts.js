126ff3db695b3c6882677c0ee127eacaaeaa4815
Julian Martinez <julianmsbar@gmail.com>
2022-09-22 10:39:45 -0300

    Segunda entrega del proyecto, modificando html.

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,71 +1,71 @@
 
-function lafuncion () {
+// function lafuncion () {
 
-let flores = [
-    ' Tulipan ',' Margarita ',' Dalia ',' Lirio '
-]
+// let flores = [
+//     ' Tulipan ',' Margarita ',' Dalia ',' Lirio '
+// ]
 
-console.log(flores)
+// console.log(flores)
 
-const arboles = ['Cedro','Pino','Acacia','Roble']
+// const arboles = ['Cedro','Pino','Acacia','Roble']
 
-let favoritos = prompt ("¿Que flores quieres agregarle a tu jardín?")
+// let favoritos = prompt ("¿Que flores quieres agregarle a tu jardín?")
 
-flores.push (favoritos)
+// flores.push (favoritos)
 
-alert ("Tus flores elegidas hasta el momento son: " + flores)
+// alert ("Tus flores elegidas hasta el momento son: " + flores)
 
-lafuncion ()
+// lafuncion ()
 
-
-}
-
-lafuncion ()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function multiplicar() {
-    
-//     const superficiePers = parseInt(document.getElementById('valor1').value)
-
-//     const tipoDeServicio = parseInt(document.getElementById('valor2').value)
-    
-//     document.getElementById('result').innerHTML = (superficiePers*tipoDeServicio); 
-
-//     console.log(superficiePers, tipoDeServicio)
 
 // }
+
+// lafuncion ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function multiplicar() {
+    
+    const superficiePers = parseInt(document.getElementById('valor1').value)
+
+    const tipoDeServicio = parseInt(document.getElementById('valor2').value)
+    
+    document.getElementById('result').innerHTML = (superficiePers*tipoDeServicio); 
+
+    console.log(superficiePers, tipoDeServicio)
+
+}
